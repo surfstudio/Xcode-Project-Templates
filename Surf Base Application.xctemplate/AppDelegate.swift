441c0ff9435837033f243e7efc5e80fab7b54dfc
Ivan Smetanin <smetanin23@yandex.ru>
2017-08-11 14:25:38 +0300

    Add files

---
+++ Surf Base Application.xctemplate/AppDelegate.swift
@@ -0,0 +1,16 @@
+//
+//  ___FILENAME___
+//  ___PACKAGENAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//___COPYRIGHT___
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+}
