17ad8489a0c34a5ed8c31af97825f82f3faa9a19
Ivan Smetanin <smetanin23@yandex.ru>
2017-08-20 12:35:17 +0300

    Fixed install directory

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-installDirectory="/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/Library/Xcode/Templates/Project Templates/iOS/Application"
+installDirectory="/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/Library/Xcode/Templates/Project Templates/iOS/User Templates"
+mkdir -p "${installDirectory}"
 cp -R "Surf Base Application.xctemplate" "${installDirectory}"
 cp -R "Surf MVP Application.xctemplate" "${installDirectory}"
